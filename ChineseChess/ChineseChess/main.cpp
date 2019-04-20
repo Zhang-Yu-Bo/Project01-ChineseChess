79b6a101f19aff4c1955415f6d9275ef568d780a
Zhang-Yu-Bo <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2019-04-20 18:31:32 +0800

    [更新] .gitignore 檔案2

--- ChineseChess/ChineseChess/main.cpp
+++ ChineseChess/ChineseChess/main.cpp
@@ -7,6 +7,7 @@ int main(void) {
 	cout << "Hello Git\n";
 	cout << "涵語說:大家加油 :3333" << endl;
 	cout << "Change IDE to Visual Studio IDE 2019 測試\n";
+	cout << "Change IDE to Visual Studio IDE 2019 測試2\n";
 	system("pause");
 	return 0;
 }
