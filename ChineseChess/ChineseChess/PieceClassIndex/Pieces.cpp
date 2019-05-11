952517700d1e2c2cbbf2a1cfaa2917345ae5151a
HiOrange428 <B10715060@mail.ntust.edu.tw>
2019-05-11 13:27:33 +0800

    [新增]中式記譜法架構，在MoveAndEat裡
    僅有架構，尚未實作

--- ChineseChess/ChineseChess/PieceClassIndex/Pieces.cpp
+++ ChineseChess/ChineseChess/PieceClassIndex/Pieces.cpp
@@ -9,32 +9,46 @@ vector<COORDINATE> Pieces::eatable(const vector<vector<int>>&) {
 }
 
 bool Pieces::MoveAndEat(COORDINATE destination, vector<vector<int>>& boardStatus, vector<vector<Pieces*>>& pieceStatus) {
+	string chineseNotation;//將替換為傳遞的reference
+	int excutor;
+	COORDINATE departure;
+	bool legalAction = false;
 	vector<COORDINATE> movePosition = movable(boardStatus);
 	vector<COORDINATE> eatPosition = eatable(boardStatus);
 	auto move = find(begin(movePosition), end(movePosition), destination);
 	auto eat = find(begin(eatPosition), end(eatPosition), destination);
-	//==============執行移棋====================
+	//=====================執行移棋=====================
 	if (move != movePosition.end()) {
+		excutor = pieceStatus[position.first][position.second]->code;
 		boardStatus[position.first][position.second] = 0;
 		pieceStatus[destination.first][destination.second] = pieceStatus[position.first][position.second];
 		pieceStatus[position.first][position.second] = NULL;
 		boardStatus[destination.first][destination.second] = code;
+		departure = position;
 		position = destination;
-		return true;
+		legalAction = true;
 	}
-	//==============執行吃棋====================
+	//=====================執行吃棋=====================
 	else if (eat != eatPosition.end()) {
+		excutor = pieceStatus[position.first][position.second]->code;
 		pieceStatus[destination.first][destination.second]->alive = false;
 		boardStatus[position.first][position.second] = 0;
 		pieceStatus[destination.first][destination.second] = pieceStatus[position.first][position.second];
 		pieceStatus[position.first][position.second] = NULL;
 		boardStatus[destination.first][destination.second] = code;
+		departure = position;
 		position = destination;
-		return true;
+		legalAction = true;
 	}
-	else {
-		return false;
+	if (legalAction) {
+		string prefix, name, surffix, action, delta;
+		//判斷程序
+
+
+
+
 	}
+	return legalAction;
 }
 //==============取回資料====================
 bool Pieces::JudgeAlive()const {
